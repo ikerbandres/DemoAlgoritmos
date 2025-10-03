46f53a9ba50e567d8b4ae03aa29852c7d3635e9b
ikerbandres <A01282986@tec.mx>
2025-10-02 18:28:42 -0600

    Primer comit

---
+++ Entidades/Planta.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAlgoritmos.Entidades
+{
+    public class Planta
+    {
+        public string IdPlanta { get; set; }
+        public int IdLote { get; set; }
+
+        public Planta(Planta planta)
+        {
+            IdPlanta = planta.IdPlanta;
+            IdLote = planta.IdLote;
+        }
+    }
+}
